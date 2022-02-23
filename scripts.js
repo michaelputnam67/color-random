8a15e02300090f59c8ece4c7b8cdf73ccc0cfd3c
Artan Myrtolli <artanmyrtolli@gmail.com>
2022-02-22 21:56:52 -0500

    Set up initial CSS styling for placeholders

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
-var characters = 'abcdef0123456789'
-var charLength = characters.length
 
-function makHex() {
+
+function makeHex() {
+  var characters = 'abcdef0123456789'
+  var charLength = characters.length
   var result = '#';
   for(i = 0; i < 6; i++) {
     result += characters.charAt(Math.floor(Math.random()* charLength))
